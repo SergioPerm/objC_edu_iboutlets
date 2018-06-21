b119d48fc0eb5cea1c19d58d5a8467cb84b79b1b
Admin <admin@mac-admin.local>
2018-06-21 21:05:02 +0300

    Initial Commit

---
+++ IBOutletTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBOutletTest
+//
+//  Created by Admin on 21.06.2018.
+//  Copyright © 2018 SergioLechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
